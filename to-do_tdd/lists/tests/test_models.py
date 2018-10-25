260faac2e044eb02ee447cb61264e06e24ab5e06
nicolas-carolo <nicolas.carolo@outlook.com>
2018-10-25 17:19:08 +0200

    tdd2.md: chapter 15 (Rewriting the Old Model Test) v.2

--- to-do_tdd/lists/tests/test_models.py
+++ to-do_tdd/lists/tests/test_models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 
 
-class ListAndItemModelsTest(TestCase):
+class ItemModelsTest(TestCase):
     
 	def test_default_text(self):
 		item = Item()
@@ -25,11 +25,6 @@ class ListAndItemModelsTest(TestCase):
 		with self.assertRaises(ValidationError):
 			item.save()
 			item.full_clean()
-
-
-	def test_get_absolute_url(self):
-		list_ = List.objects.create()
-		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
 
 	def test_duplicate_items_are_invalid(self):
@@ -62,5 +57,13 @@ class ListAndItemModelsTest(TestCase):
 	def test_string_representation(self):
 		item = Item(text='some text')
 		self.assertEqual(str(item), 'some text')
+
+
+
+class ListModelTest(TestCase):
+
+	def test_get_absolute_url(self):
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
 
 
